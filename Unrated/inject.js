928f5cacecd177d5229bef279c26f3d124cfc1d9
Michael Bortnyck <michael.bortnyck@gmail.com>
2016-12-03 01:56:37 -0600

    options page with persistence

--- Unrated/inject.js
+++ Unrated/inject.js
@@ -1,35 +1,20 @@
-var style = document.createElement('link');
-style.rel = 'stylesheet';
-style.type = 'text/css';
-switch (window.location.hostname) {
-  case "www.avclub.com":
-    style.href = chrome.extension.getURL('styles/avclub.css');
-    break;
-  case "www.gamespot.com":
-    style.href = chrome.extension.getURL('styles/gamespot.css');
-    break;
-  case "www.ign.com":
-    style.href = chrome.extension.getURL('styles/ign.css');
-    break;
-  case "www.nme.com":
-    style.href = chrome.extension.getURL('styles/nme.css');
-    break;
-  case "pitchfork.com":
-    style.href = chrome.extension.getURL('styles/pitchfork.css');
-    break;
-  case "www.residentadvisor.net":
-    if (window.location.pathname == "/reviews.aspx") {
-      style.href = chrome.extension.getURL('styles/residentadvisor_reviews.css');
-    }
-    else {
-      style.href = chrome.extension.getURL('styles/residentadvisor.css');
-    }
-    break;
-  case "www.rollingstone.com":
-    style.href = chrome.extension.getURL('styles/rollingstone.css');
-    break;
-  case "www.xlr8r.com":
-    style.href = chrome.extension.getURL('styles/xlr8r.css');
-    break;
+function inject(css) {
+  var style = document.createElement('link');
+  style.rel = 'stylesheet';
+  style.type = 'text/css';
+  style.href = chrome.extension.getURL(css);
+  (document.head||document.documentElement).appendChild(style);
 }
-(document.head||document.documentElement).appendChild(style);
+
+var pathArray = window.location.hostname.split('.');
+if (pathArray[0] == "www") {
+  host = pathArray[1];
+}
+else {
+  host = pathArray[0];
+}
+chrome.storage.sync.get(host, function(val) {
+  if (val[host]) {
+    inject('styles/' + host + '.css');
+  }
+});
